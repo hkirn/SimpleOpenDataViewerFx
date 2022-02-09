1f61a170328043119a8e09bfcd00e3992d5b0f5b
Holger Kirn <hkirn@stud.hs-heilbronn.de>
2022-02-09 22:18:35 +0100

    test commit for CI Pipeline with fixed error

--- src/main/java/com/example/simpleopendataviewerfx/HaltestelleApplication.java
+++ src/main/java/com/example/simpleopendataviewerfx/HaltestelleApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HaltestelleApplicatio extends Application {
+public class HaltestelleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HaltestelleApplication.class.getResource("haltestelle-view.fxml"));
